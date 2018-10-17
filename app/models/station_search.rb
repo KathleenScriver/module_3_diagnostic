4ae3423a9753a6ebfe27728c1e795e63a119adce
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-17 10:39:33 -0600

    Add two searches, one for propane, one for electric since the 10 closest stations were all electric and I was not getting anny propane stations in my view

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -9,9 +9,13 @@ class StationSearch
       faraday.adapter Faraday.default_adapter
     end
 
-    response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=ELEC&fuel_type=LPG&limit=#{limit}")
+    propane_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=LPG&limit=#{limit / 2}")
+    electric_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=ELEC&limit=#{limit / 2}")
 
-    station_data = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
+    propane_data = JSON.parse(propane_response.body, symbolize_names: true)[:fuel_stations]
+    electric_data = JSON.parse(electric_response.body, symbolize_names: true)[:fuel_stations]
+
+    station_data = propane_data.zip(electric_data).flatten
 
     station_data.map do |raw_station|
       Station.new(raw_station)
