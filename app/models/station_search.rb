d83fcce798e14fac32f6270e27cbf02ff5de6207
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-17 10:54:53 -0600

    More refactors in service

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -6,19 +6,6 @@ class StationSearch
   def closest_stations(limit = 10)
     station_data = NrelService.new(@zip_code).closest_stations(limit)
 
-    # conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-    #   faraday.params["api_key"] = ENV["nrel_key"]
-    #   faraday.adapter Faraday.default_adapter
-    # end
-
-    # propane_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=LPG&limit=#{limit / 2}")
-    # electric_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=ELEC&limit=#{limit / 2}")
-
-    # propane_data = JSON.parse(propane_response.body, symbolize_names: true)[:fuel_stations]
-    # electric_data = JSON.parse(electric_response.body, symbolize_names: true)[:fuel_stations]
-
-    # station_data = propane_data.zip(electric_data).flatten
-
     station_data.map do |raw_station|
       Station.new(raw_station)
     end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -3,23 +3,26 @@ class NrelService
     @zip_code = zip_code
   end
 
-def closest_stations(limit)
-  propane_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=LPG&limit=#{limit / 2}")
-  electric_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=ELEC&limit=#{limit / 2}")
-  propane_data = JSON.parse(propane_response.body, symbolize_names: true)[:fuel_stations]
-  electric_data = JSON.parse(electric_response.body, symbolize_names: true)[:fuel_stations]
-  station_data = propane_data.zip(electric_data).flatten
-
-end
-
-def conn
-  Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-    faraday.params["api_key"] = ENV["nrel_key"]
-    faraday.adapter Faraday.default_adapter
+  def closest_stations(limit)
+    propane_data = propane_stations(limit)
+    electric_data = electric_stations(limit)
+    station_data = propane_data.zip(electric_data).flatten
   end
-end
 
+  def propane_stations(limit)
+    propane_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=LPG&limit=#{limit / 2}")
+    JSON.parse(propane_response.body, symbolize_names: true)[:fuel_stations]
+  end
 
+  def electric_stations(limit)
+    electric_response = conn.get("/api/alt-fuel-stations/v1/nearest.json?location=#{@zip_code}&radius=10.0&fuel_type=ELEC&limit=#{limit / 2}")
+    JSON.parse(electric_response.body, symbolize_names: true)[:fuel_stations]
+  end
 
-
+  def conn
+    Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.params["api_key"] = ENV["nrel_key"]
+      faraday.adapter Faraday.default_adapter
+    end
+  end
 end
